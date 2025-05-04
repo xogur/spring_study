0cedf6d4f8bb37004146d8d037586e8b3bf9b873
Your Name <you@example.com>
2025-05-04 22:18:04 +0900

    주석

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,15 +2,21 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
+    // MemberRepository의 기능들을 구현한 MemoryMemberRepository객체를 생성하는 memberRepository
+    // MemberRepository는 그냥 객체 타입을 선언
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
+    // 구현체로서 인터페이스에 있는 기능들 재정의
+    // 회원가입 재정의
     @Override
     public void join(Member member) {
+        // MemoryMemberRepository인 memberRepository에 회원 정보 저장
         memberRepository.save(member);
     }
 
     @Override
     public Member findMember(Long memberId) {
+        // memberId를 이용해 회원 조회
         return memberRepository.findById(memberId);
     }
 }
--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -5,15 +5,20 @@ import java.util.Map;
 
 public class MemoryMemberRepository implements MemberRepository {
 
+    // static은 해당 모든 객체들이 같은 메모리 공간을 공유함.
+    // 예를들어 member1객체에 권태혁 이라는 이름을 넣으면
+    // member2객체에서도 권태혁 이라는 이름을 조회 가능
     private static Map<Long, Member> store = new HashMap<>();
 
     @Override
     public void save(Member member) {
+        // store에 삽입 (삽입하려는 회원의 회원 넘버, 회원 정보)
         store.put(member.getId(), member);
     }
 
     @Override
     public Member findById(Long memberId) {
+        // store객체는 hashmap타입이기 때문에 키값인 회원id를 이용해 회원 정보를 조회
         return store.get(memberId);
     }
 }
